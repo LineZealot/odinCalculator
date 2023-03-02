219101439fddf29e732cf36108d329e422268969
LineZealot <holdenmountain@gmail.com>
2023-03-02 08:53:52 -0600

    Operate function for all operation cases and display shows 0 on start

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-/* Write a function for each kind of operation */
+function calculator() {
+    // Start calculator with 0 on screen
+    const display = document.querySelector('.calculator-display');
+    function startDisplay() {
+        display.textContent = 0;
+    };
+    startDisplay();
+
+    const btnOne = document.querySelector('.button.one');
+    function operate(num1, op, num2) {
+        switch(op) {
+            case 'add':
+                return(num1 + num2);
+                break;
+            case 'sub':
+                return(num1 - num2);
+                break;
+            case 'mult':
+                return(num1 * num2);
+                break;
+            case 'div':
+                return(num1 / num2);
+                break;
+        }
+    }
+    console.log(operate(4, 'sub', 2))
+}
+calculator();
