f52afbadd606b1d83db83c6b6247707ab587e0c1
Holden Smith <105118057+LineZealot@users.noreply.github.com>
2023-03-04 12:01:29 -0600

    Better number buttons script.js
    Number buttons have built in values and functions for more efficient editing

--- script.js
+++ script.js
@@ -7,104 +7,158 @@ function calculator() {
     };
     startDisplay();
 
+    // Inputs
+    const numArr1 = [];
+    const numArr2 = [];
+    let opSelector = '';
+
     // Number buttons
     const buttonOne = document.querySelector('.button.one');
     buttonOne.addEventListener('click', function(){
+        const value = 1;
+        const pushVal = (n) => numArr1.push(n);
+
         if(displayState === 0) {
-            display.textContent = 1;
+            display.textContent = value;
             displayState++;
+            pushVal(value);
         } else if(displayState < 10) {
-            display.textContent += 1;
+            display.textContent += value;
             displayState++;
+            pushVal(value);
         }
     });
     const buttonTwo = document.querySelector('.button.two');
     buttonTwo.addEventListener('click', function(){
+        const value = 2;
+        const pushVal = (n) => numArr1.push(n);
+
         if(displayState === 0) {
-            display.textContent = 2;
+            display.textContent = value;
             displayState++;
+            pushVal(value);
         } else if(displayState < 10) {
-            display.textContent += 2;
+            display.textContent += value;
             displayState++;
+            pushVal(value);
         }
     });
     const buttonThree = document.querySelector('.button.three');
     buttonThree.addEventListener('click', function(){
+        const value = 3;
+        const pushVal = (n) => numArr1.push(n);
+
         if(displayState === 0) {
-            display.textContent = 3;
+            display.textContent = value;
             displayState++;
+            pushVal(value);
         } else if(displayState < 10) {
-            display.textContent += 3;
+            display.textContent += value;
             displayState++;
+            pushVal(value);
         }
     });
     const buttonFour = document.querySelector('.button.four');
     buttonFour.addEventListener('click', function(){
+        const value = 4;
+        const pushVal = (n) => numArr1.push(n);
+
         if(displayState === 0) {
-            display.textContent = 4;
+            display.textContent = value;
             displayState++;
+            pushVal(value);
         } else if(displayState < 10) {
-            display.textContent += 4;
+            display.textContent += value;
             displayState++;
+            pushVal(value);
         }
     });
     const buttonFive = document.querySelector('.button.five');
     buttonFive.addEventListener('click', function(){
+        const value = 5;
+        const pushVal = (n) => numArr1.push(n);
+
         if(displayState === 0) {
-            display.textContent = 5;
+            display.textContent = value;
             displayState++;
+            pushVal(value);
         } else if(displayState < 10) {
-            display.textContent += 5;
+            display.textContent += value;
             displayState++;
+            pushVal(value);
         }
     });
     const buttonSix = document.querySelector('.button.six');
     buttonSix.addEventListener('click', function(){
+        const value = 6;
+        const pushVal = (n) => numArr1.push(n);
+
         if(displayState === 0) {
-            display.textContent = 6;
+            display.textContent = value;
             displayState++;
+            pushVal(value);
         } else if(displayState < 10) {
-            display.textContent += 6;
+            display.textContent += value;
             displayState++;
+            pushVal(value);
         }
     });
     const buttonSeven = document.querySelector('.button.seven');
     buttonSeven.addEventListener('click', function(){
+        const value = 7;
+        const pushVal = (n) => numArr1.push(n);
+
         if(displayState === 0) {
-            display.textContent = 7;
+            display.textContent = value;
             displayState++;
+            pushVal(value);
         } else if(displayState < 10) {
-            display.textContent += 7;
+            display.textContent += value;
             displayState++;
+            pushVal(value);
         }
     });
     const buttonEight = document.querySelector('.button.eight');
     buttonEight.addEventListener('click', function(){
+        const value = 8;
+        const pushVal = (n) => numArr1.push(n);
+
         if(displayState === 0) {
-            display.textContent = 8;
+            display.textContent = value;
             displayState++;
+            pushVal(value);
         } else if(displayState < 10) {
-            display.textContent += 8;
+            display.textContent += value;
             displayState++;
+            pushVal(value);
         }
     });
     const buttonNine = document.querySelector('.button.nine');
     buttonNine.addEventListener('click', function(){
+        const value = 9;
+        const pushVal = (n) => numArr1.push(n);
+
         if(displayState === 0) {
-            display.textContent = 9;
+            display.textContent = value;
             displayState++;
+            pushVal(value);
         } else if(displayState < 10) {
-            display.textContent += 9;
+            display.textContent += value;
             displayState++;
+            pushVal(value);
         }
     });
     const buttonZero = document.querySelector('.button.zero');
     buttonZero.addEventListener('click', function(){
+        const value = 0;
+        const pushVal = (n) => numArr1.push(n);
+
         if(displayState === 0) {
-            display.textContent = 0;
+            return;
         } else if(displayState < 10) {
-            display.textContent += 0;
+            display.textContent += value;
             displayState++;
+            pushVal(value);
         }
     });
     
@@ -113,7 +167,14 @@ function calculator() {
     buttonClear.addEventListener('click', () => {
         display.textContent = 0;
         displayState = 0;
-    })
+        numArr1.length = 0;
+    });
+
+    // Operator Buttons
+    const buttonEquals = document.querySelector('.button.equal');
+    buttonEquals.addEventListener('click', () => {
+        console.log(parseInt(numArr1.join('')));
+    });
 
     // Operating function
     function operate(num1, op, num2) {
